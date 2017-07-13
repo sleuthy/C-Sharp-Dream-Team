4892f65a51ddd8bdedd2c323f04b3195dfa2ab4c
Kathleen Weisensel <kweisensel@gmail.com>
2017-07-13 15:41:11 -0500

    dreamteam interface and classes

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+    public interface IDreamTeam
+    {
+        string Speciality { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        void Work();
+    }
+
+    public class Jackie : IDreamTeam
+    {
+        public string Speciality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Work() {
+            Console.WriteLine("");
+        }
+    }
+
+     public class Madeline : IDreamTeam
+    {
+        public string Speciality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Work() {
+
+        }
+    }
+
+     public class Eliza : IDreamTeam
+    {
+        public string Speciality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Work() {
+
+        }
+    }
+
+     public class Tamela : IDreamTeam
+    {
+        public string Speciality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Work() {
+
+        }
+    }
+
+     public class Chaz : IDreamTeam
+    {
+        public string Speciality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Work() {
+
+        }
+    }
+
+    public class Kathy : IDreamTeam
+    {
+        public string Speciality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Work() {
+
+        }
+    }
+
+   
+
+namespace dreamteam
+
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+            
+            
+            serverSide.Add(ed);
+            serverSide.Add(eliza);
+            serverSide.Add(kathy);
+
+            clientSide.Add(preeti);
+            clientSide.Add(mitchell);
+            clientSide.Add(matt);
+        }
+    }
+}
